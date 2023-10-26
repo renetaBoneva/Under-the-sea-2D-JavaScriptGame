8a73af738a97f86289b1d9d1ac38444870c20d28
Ренета Бонева <reneta.boneva@abv.bg>
2023-10-26 12:20:26 +0300

    Add animation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Canvas setup
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-canvas.width = 500;
+canvas.width = 800;
 canvas.height = 500;
 
 let score = 0;
@@ -16,14 +16,13 @@ const mouse = {
     click: false
 }
 
-canvas.addEventListener('mousedown', (event) => {
+canvas.addEventListener('mousedown', function (event) {
     mouse.click = true;
     mouse.X = event.x - canvasPosition.left;
     mouse.Y = event.y - canvasPosition.top;
-    console.log(mouse.X, mouse.Y);
 })
-canvas.addEventListener('mouseup', (event) => {
-
+canvas.addEventListener('mouseup', function (event) {
+    mouse.click = false;
 })
 
 // Player
@@ -41,30 +40,38 @@ class Player {
     }
 
     update() {
-        const dx = this.x - mouse.x;
-        const dy = this.y - mouse.y;
+        const dx = this.x - mouse.X;
+        const dy = this.y - mouse.Y;
         if (mouse.X != this.x) {
-            this.x -= dx/30;
+            this.x -= dx / 30;
         }
 
         if (mouse.Y != this.y) {
-            this.y -= dy/30;
+            this.y -= dy / 30;
         }
     }
-    draw(){
-        if(mouse.click) {
+    draw() {
+        if (mouse.click) {
             ctx.lineWidth = 0.2;
             ctx.beginPath();
             ctx.moveTo(this.x, this.y);
-            ctx.lineTo(mouse.X,mouse.Y);
-            ctx.stroke(); 
+            ctx.lineTo(mouse.X, mouse.Y);
+            ctx.stroke();
         }
         ctx.fillStyle = "red";
         ctx.beginPath();
-        ctx.arc(this.x,this,y,0, Math.PI*2);
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
+        ctx.fillRect(this.x, this.y, this.radius, 10);
     }
 }
+const player1 = new Player();
 // Bubbles
 // Animation Loop
+function animate() {
+    player1.update();
+    player1.draw();
+    requestAnimationFrame(animate);
+}
+animate()
